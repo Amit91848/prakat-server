7a7ff7e239b82a04680b602d886298d607368a81
Amit Sharma <amitsharma@Amits-MacBook-Air.local>
2023-08-08 13:08:01 +0530

    tags filtering

--- router/search.py
+++ router/search.py
@@ -5,6 +5,7 @@ from service.search_services import load_and_vectorize_data, rank_documents
 from pathlib import Path
 from models.CrawledSites import crawled_sites
 from pymongo import MongoClient
+from typing import Annotated
 
 router = APIRouter()
 
@@ -14,8 +15,10 @@ client = MongoClient(
 db = client['prakat23']
 crawled_sites = db['crawled_sites']
 
+collection = crawled_sites.find().limit(800)
+
 # collection = await crawled_sites.find_all().to_di()
-collection = crawled_sites.find()
+# collection = crawled_sites.find()
 df, vectorizer, vector_tfidf = load_and_vectorize_data(collection)
 
 # pipeline = [
@@ -34,13 +37,16 @@ unique_tags = ['Heroin', 'Stimulants', 'Revolvers', 'Marijuana', 'Phone Number',
 
 
 @router.get("/")
-async def search(q: str = Query(min_length=1), page: int = Query(1, gt=0), pageCount: int = Query(20, ge=1)):
+async def search(q: Annotated[str | None, Query(min_length=1)] = None, page: int = Query(1, gt=0), pageCount: int = Query(20, ge=1), tags: Annotated[str | None, Query()] = None):
     # print(q)
-    try:
-        results = rank_documents(
-            q, vectorizer, vector_tfidf, df, page, pageCount)
-        # return await crawled_sites.find_all().limit(100).to_list()
-        return results
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=400, detail=str(e))
+    if q:
+        try:
+            results = rank_documents(
+                q, vectorizer, vector_tfidf, df, page, pageCount, tags)
+            # return await crawled_sites.find_all().limit(100).to_list()
+            return results
+        except Exception as e:
+            print(e)
+            raise HTTPException(status_code=400, detail=str(e))
+    else:
+        return []
--- service/search_services.py
+++ service/search_services.py
@@ -6,31 +6,37 @@ from sklearn.metrics.pairwise import linear_kernel
 import json
 # from models.CrawledSites import crawled_sites
 
+
 def get_lines(file_obj):
     json_list = []
     for line in file_obj:
         json_line = json.loads(line.strip())
         json_list.append(json_line)
-    
+
     return json_list
+
 
 def load_and_vectorize_data(collection):
     df = pd.DataFrame(collection)
     vectorizer = TfidfVectorizer()
     vectorizer.fit(df['body'])
     vector_tfidf = vectorizer.transform(df['body'])
-    
+
     return df, vectorizer, vector_tfidf
 
-def rank_documents(query: str, vectorizer: TfidfVectorizer, vector_tfidf: TfidfTransformer, df, page: int, pageCount: int) -> List[SearchResult]:
+
+def rank_documents(query: str, vectorizer: TfidfVectorizer, vector_tfidf: TfidfTransformer, df, page: int, pageCount: int, tags: str | None) -> List[SearchResult]:
     query_vec = vectorizer.transform([query])
     cosine_similarities = linear_kernel(query_vec, vector_tfidf).flatten()
     ranking = cosine_similarities.argsort()[::-1]
-    print(ranking)
 
     start_index = (page - 1) * pageCount
     end_index = start_index + pageCount
-    
+
+    results = []
+    start_index = (page - 1) * pageCount
+    end_index = start_index + pageCount
+
     results = []
     count = 1
     for i in ranking[start_index:end_index]:
@@ -39,9 +45,43 @@ def rank_documents(query: str, vectorizer: TfidfVectorizer, vector_tfidf: TfidfT
         # If tags_list is not a list, set it to an empty list
         if not isinstance(tags_list, list):
             tags_list = []
-        result = SearchResult(title=df['title'][i], url=df['url'][i], score=ranking[i], id=str(df['_id'][i]), tags=tags_list)
 
-        results.append(result)
-        count+=1
+        if tags:
+            # Check if the filter is present in tags_list
+            # if tags in tags_list:
+            #     result = SearchResult(title=df['title'][i], url=df['url'][i], score=ranking[i], id=str(df['_id'][i]), tags=tags_list)
+            #     results.append(result)
+            # else:
+            #     end_index+=1
+            #     continue
+            # Convert comma-separated string to a list
+            tags_to_check = tags.split(',')
+
+            # Check if any tag in tags_to_check is present in tags_list
+            if any(tag in tags_list for tag in tags_to_check):
+                result = SearchResult(title=df['title'][i], url=df['url'][i], score=ranking[i], id=str(
+                    df['_id'][i]), tags=tags_list)
+                results.append(result)
+            else:
+                end_index += 1
+                continue
+        else:
+            # No filter specified, include the document in the results
+            result = SearchResult(title=df['title'][i], url=df['url'][i], score=ranking[i], id=str(
+                df['_id'][i]), tags=tags_list)
+            results.append(result)
+
+        # Increment end_index by one regardless of the filter being present or not
+        count += 1
 
     return results
+
+# count = 1
+# for i in ranking[start_index:end_index]:
+#     tags_list = df['tags'][i]
+
+#     if not isinstance(tags_list, list):
+#         tags_list = []
+#     result = SearchResult(title=df['title'][i], url=df['url'][i], score=ranking[i], id=str(df['_id'][i]), tags=tags_list)
+#     results.append(result)
+#     count+=1
