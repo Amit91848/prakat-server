2ee0caede2b84b665488f99a00b8831de145f385
Amit Sharma <amitsharma@Amits-MacBook-Air.local>
2023-08-08 14:41:21 +0530

    pickle dump

--- router/search.py
+++ router/search.py
@@ -5,7 +5,9 @@ from service.search_services import load_and_vectorize_data, rank_documents
 from pathlib import Path
 from models.CrawledSites import crawled_sites
 from pymongo import MongoClient
+import pickle
 from typing import Annotated
+import time
 
 router = APIRouter()
 
@@ -15,11 +17,24 @@ client = MongoClient(
 db = client['prakat23']
 crawled_sites = db['crawled_sites']
 
-collection = crawled_sites.find().limit(800)
+
+# collection = list(crawled_sites.find())
 
 # collection = await crawled_sites.find_all().to_di()
 # collection = crawled_sites.find()
-df, vectorizer, vector_tfidf = load_and_vectorize_data(collection)
+# df, vectorizer, vector_tfidf = load_and_vectorize_data(collection)
+
+df_pickle = "df.pickle"
+vectorizer_pickle = "vectorizer.pickle"
+vector_tfidf_pickle = "vector_tfidf.pickle"
+
+# # Open the file in binary write mode and pickle the data
+# with open(df_pickle, 'wb') as file:
+#     pickle.dump(df, file)
+# with open(vectorizer_pickle, 'wb') as file:
+#     pickle.dump(vectorizer, file)
+# with open(vector_tfidf_pickle, 'wb') as file:
+#     pickle.dump(vector_tfidf, file)
 
 # pipeline = [
 #     {"$unwind": "$tags"},
@@ -40,6 +55,17 @@ unique_tags = ['Heroin', 'Stimulants', 'Revolvers', 'Marijuana', 'Phone Number',
 async def search(q: Annotated[str | None, Query(min_length=1)] = None, page: int = Query(1, gt=0), pageCount: int = Query(20, ge=1), tags: Annotated[str | None, Query()] = None):
     # print(q)
     if q:
+        with open(df_pickle, 'rb') as file:
+            df = pickle.load(file)
+
+        with open(vector_tfidf_pickle, 'rb') as file:
+            vector_tfidf = pickle.load(file)
+
+        with open(vectorizer_pickle, 'rb') as file:
+            vectorizer = pickle.load(file)
+
+        # print("Loaded data:", loaded_data)
+
         try:
             results = rank_documents(
                 q, vectorizer, vector_tfidf, df, page, pageCount, tags)
